7db6d83c89dd35427f91439a9cacedd426cbe0f8
Clint Chang <clintchang@gmail.com>
2014-11-10 23:24:10 -0500

    finished code

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var enterAgeField: UITextField!
+    
+    @IBOutlet weak var ageResultsField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBAction func submitButton(sender: UIButton) {
+        var age = enterAgeField.text.toInt()! * 7
+        
+        enterAgeField.text = ""
+        enterAgeField.resignFirstResponder()
+        
+        
+        ageResultsField.hidden = false
+        ageResultsField.text = "Laika is " + "\(age)" + " in human years"
+    
+        
+        
+    }
 
 }
 
